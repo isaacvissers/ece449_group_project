79de41d97360c3ae6d36c09c0593c1f0783d1d06
Isaac Vissers <ivissers@uaberta.ca>
2024-11-26 14:02:27 -0700

    Added my controller class, so far it is the same as Dr. Dicks

--- scenario_test.py
+++ scenario_test.py
@@ -5,6 +5,7 @@ import time
 from kesslergame import Scenario, KesslerGame, GraphicsType
 from test_controller import TestController
 from scott_dick_controller import ScottDickController
+from my_controller import MyController
 from graphics_both import GraphicsBoth
 my_test_scenario = Scenario(name='Test Scenario',
  num_asteroids=5,
@@ -23,7 +24,7 @@ game_settings = {'perf_tracker': True,
 game = KesslerGame(settings=game_settings) # Use this to visualize the game scenario
 # game = TrainerEnvironment(settings=game_settings) # Use this for max-speed, no-graphics simulation
 pre = time.perf_counter()
-score, perf_data = game.run(scenario=my_test_scenario, controllers = [TestController(), ScottDickController()])
+score, perf_data = game.run(scenario=my_test_scenario, controllers = [TestController(), MyController()])
 print('Scenario eval time: '+str(time.perf_counter()-pre))
 print(score.stop_reason)
 print('Asteroids hit: ' + str([team.asteroids_hit for team in score.teams]))
